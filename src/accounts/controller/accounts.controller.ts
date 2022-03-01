f4dc1569b55dba0718b6f0607549da5783df381e
GwendalBroudin-Emoko <gwendal.broudin@emoko.io>
2022-03-01 15:57:29 +0100

    use own fork of nestjs-mongoose-crud

--- src/accounts/controller/accounts.controller.ts
+++ src/accounts/controller/accounts.controller.ts
@@ -4,22 +4,18 @@ import { ModelType, ReturnModelType } from '@typegoose/typegoose/lib/types';
 import { InjectModel } from 'nestjs-typegoose';
 import { AccountsService } from '../services/accounts/accounts.service';
 import { Crud } from 'nestjs-mongoose-crud';
-import { CrudController } from 'nestjs-mongoose-crud/dist/crud.controller';
+// import { CrudController } from 'nestjs-mongoose-crud/dist/crud.controller';
 
-// @Crud({
-//   model: AccountEntity
-// })
+@Crud({
+  model: AccountEntity
+})
 @Controller('accounts')
-export class AccountsController extends CrudController {
+export class AccountsController  {
   constructor(
     @InjectModel(AccountEntity)
     public readonly model: ReturnModelType<typeof AccountEntity>,
     private readonly accountService: AccountsService
   ) {
-    super(model, null);
   }
 
-  public override config(req: any): Promise<any> {
-    return super.config(req);
-  }
 }
